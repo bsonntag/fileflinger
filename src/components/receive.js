27ac48c24151e8268ace17ec47a525f89cd31de4
Benjamim Sonntag <benjamimsonntag@gmail.com>
2018-09-23 18:59:35 +0100

    Update Receive and ConnectionReceiver components

--- src/components/receive.js
+++ src/components/receive.js
@@ -1,76 +1,47 @@
 import ConnectionReceiver from './connection-receiver';
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
+import ReceivingPeer from './receiving-peer';
 
-class Receive extends Component {
-
-  state = {
-    blob: null,
-    file: null,
-    peer: null,
-  }
-
-  handleConnection = peer => {
-    this.setState({ peer });
-
-    peer.on('data', data => {
-      const { file } = this.state;
-
-      if (!file) {
-        this.setState({ file: JSON.parse(String(data)) });
-        peer.send('ok');
-
-        return;
+const Receive = () => (
+  <ReceivingPeer>
+    {({ blob, code, connected, file, setRemoteCode }) => {
+      if (!connected) {
+        return (
+          <ConnectionReceiver
+            code={code}
+            onRemoteCode={setRemoteCode}
+          />
+        );
       }
 
-      const blob = new Blob([data], { type: 'application/octet-stream' });
+      return (
+        <div>
+          {!file ? 'Waiting...' : (
+            <Fragment>
+              <div>
+                {'File: '}
+                {file.name}
+              </div>
 
-      this.setState({ blob });
-    });
-  }
+              <div>
+                {'Size: '}
+                {file.size}
+              </div>
+            </Fragment>
+          )}
 
-  componentWillUnmount() {
-    const { peer } = this.state;
-
-    if (peer) {
-      peer.destroy();
-    }
-  }
-
-  render() {
-    const { blob, file, peer } = this.state;
-
-    if (!peer) {
-      return <ConnectionReceiver onConnected={this.handleConnection} />;
-    }
-
-    return (
-      <div>
-        {!file ? 'Waiting...' : (
-          <Fragment>
-            <div>
-              {'File: '}
-              {file.name}
-            </div>
-
-            <div>
-              {'Size: '}
-              {file.size}
-            </div>
-          </Fragment>
-        )}
-
-        {blob && (
-          <a
-            download={file.name}
-            href={URL.createObjectURL(blob)}
-          >
-            {'Download'}
-          </a>
-        )}
-      </div>
-    );
-  }
-
-}
+          {blob && (
+            <a
+              download={file.name}
+              href={URL.createObjectURL(blob)}
+            >
+              {'Download'}
+            </a>
+          )}
+        </div>
+      );
+    }}
+  </ReceivingPeer>
+);
 
 export default Receive;
